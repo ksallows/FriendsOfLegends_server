85e01a1a4f570780332d67f5e57f477e4745c61a
Katie Sallows <ktsallows@gmail.com>
2022-01-23 11:02:39 -0500

    DRY fix for update and refresh

--- controllers/profilecontroller.js
+++ controllers/profilecontroller.js
@@ -4,6 +4,71 @@ const validateJWT = require('../middleware/validatejwt');
 const util = require('util');
 const { request, response } = require('express');
 
+const refresh = async (summonerName, server) => {
+    let data = {
+        summonerId: null,
+        level: null,
+        summonerIcon: null,
+        rank: null,
+        topChamps: [],
+    }
+
+    await fetch(`https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'X-Riot-Token': process.env.RIOT_API_KEY
+        }
+    })
+        .then(result => result.json())
+        .then(result => {
+            if (result.status_code === 404) {
+                return response.status(500).json({
+                    message: "Summoner not found",
+                })
+            }
+            data.summonerId = result.id;
+            data.summonerIcon = result.profileIconId;
+            data.level = result.summonerLevel
+        })
+
+    await fetch(`https://${server}.api.riotgames.com/lol/league/v4/entries/by-summoner/${data.summonerId}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'X-Riot-Token': process.env.RIOT_API_KEY
+        }
+    })
+        .then(result => result.json())
+        .then(result => {
+            if (result.length === 0)
+                data.rank = 'Unranked';
+            else {
+                let a;
+                for (i = 0; i < result.length; i++) {
+                    if (result[i].queueType === 'RANKED_SOLO_5x5') {
+                        a = i;
+                    }
+                }
+                if (result[a].tier !== 'CHALLENGER' && result[a].tier !== 'MASTER' && result[a].tier !== 'GRANDMASTER')
+                    data.rank = `${result[a].tier === ''} ${result[a].rank}`
+                else
+                    data.rank = result[a].tier
+            }
+        })
+
+    await fetch(`https://${server}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${data.summonerId}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+            'X-Riot-Token': process.env.RIOT_API_KEY
+        }
+    })
+        .then(result => result.json())
+        .then(result => data.topChamps.push(result[0].championId, result[1].championId, result[2].championId))
+    return data
+}
+
 // *
 // *    find profile by id
 // *
@@ -56,26 +121,8 @@ router.put('/update', validateJWT, async (request, response) => {
 
     let {
         summonerName,
-        server,
-        discord
+        server
     } = request.body.profile;
-
-    const newData = {
-        summonerId: null,
-        level: null,
-        summonerIcon: null,
-        discord: discord,
-        roles: null,
-        active: null,
-        gameModes: null,
-        voiceComm: null,
-        description: null,
-        active: true,
-        rank: null,
-        topChamps: [],
-        summonerName: summonerName,
-        server: server
-    }
 
     if (!summonerName)
         return response.status(500).json({ message: 'Summoner Name is Required' })
@@ -83,66 +130,16 @@ router.put('/update', validateJWT, async (request, response) => {
     if (!server)
         return response.status(500).json({ message: 'Server is Required' })
 
-    if (!discord.match(/^.{3,32}#[0-9]{4}$/))
+    if (!request.body.profile.discord.match(/^.{3,32}#[0-9]{4}$/))
         return response.status(400).json({ message: "Discord tag not valid" });
 
     summonerName = encodeURI(summonerName);
 
     const accountId = request.accountId;
 
-    await fetch(`https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-Riot-Token': process.env.RIOT_API_KEY
-        }
-    })
-        .then(result => result.json())
-        .then(result => {
-            if (result.status_code === 404) {
-                return response.status(500).json({
-                    message: "Summoner not found",
-                })
-            }
-            newData.summonerId = result.id;
-            newData.summonerIcon = result.profileIconId;
-            newData.level = result.summonerLevel
-        })
+    let newData = await refresh(summonerName, server)
 
-    await fetch(`https://${server}.api.riotgames.com/lol/league/v4/entries/by-summoner/${newData.summonerId}`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-Riot-Token': process.env.RIOT_API_KEY
-        }
-    })
-        .then(result => result.json())
-        .then(result => {
-            if (result.length === 0)
-                newData.rank = 'Unranked';
-            else {
-                let a;
-                for (i = 0; i < result.length; i++) {
-                    if (result[i].queueType === 'RANKED_SOLO_5x5') {
-                        a = i;
-                    }
-                }
-                if (result[a].tier !== 'CHALLENGER' && result[a].tier !== 'MASTER' && result[a].tier !== 'GRANDMASTER')
-                    newData.rank = `${result[a].tier === ''} ${result[a].rank}`
-                else
-                    newData.rank = result[a].tier
-            }
-        })
-
-    await fetch(`https://${server}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${newData.summonerId}`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-Riot-Token': process.env.RIOT_API_KEY
-        }
-    })
-        .then(result => result.json())
-        .then(result => newData.topChamps.push(result[0].championId, result[1].championId, result[2].championId))
+    newData = Object.assign(newData, request.body.profile)
 
     try {
         await Profile.update(
@@ -151,6 +148,7 @@ router.put('/update', validateJWT, async (request, response) => {
         );
         response.status(200).json({
             message: "Updated",
+            data: newData
         });
     } catch (error) {
         response.status(500).json({
@@ -164,88 +162,25 @@ router.put('/update', validateJWT, async (request, response) => {
 // *
 router.put('/refresh', validateJWT, async (request, response) => {
 
-    const newData = {
-        summonerId: null,
-        level: null,
-        summonerIcon: null,
-        rank: null,
-        topChamps: [],
-    }
-
-    let summonerName, server;
+    let profile;
 
     const accountId = request.accountId;
 
     try {
-        const profile = await Profile.findOne({
+        profile = await Profile.findOne({
             where: {
                 accountId: accountId
             },
             attributes: ['summonerId', 'summonerName', 'server'],
             raw: true
         })
-        summonerId = profile.summonerId;
-        summonerName = profile.summonerName;
-        server = profile.server;
     } catch (error) {
         response.status(500).json({
             error: `Error: ${error}`
         })
     }
 
-    await fetch(`https://${server}.api.riotgames.com/lol/summoner/v4/summoners/by-name/${summonerName}`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-Riot-Token': process.env.RIOT_API_KEY
-        }
-    })
-        .then(result => result.json())
-        .then(result => {
-            if (result.status_code === 404) {
-                return response.status(500).json({
-                    message: "Summoner not found",
-                })
-            }
-            newData.summonerId = result.id;
-            newData.summonerIcon = result.profileIconId;
-            newData.level = result.summonerLevel
-        })
-
-    await fetch(`https://${server}.api.riotgames.com/lol/league/v4/entries/by-summoner/${newData.summonerId}`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-Riot-Token': process.env.RIOT_API_KEY
-        }
-    })
-        .then(result => result.json())
-        .then(result => {
-            if (result.length === 0)
-                newData.rank = 'Unranked';
-            else {
-                let a;
-                for (i = 0; i < result.length; i++) {
-                    if (result[i].queueType === 'RANKED_SOLO_5x5') {
-                        a = i;
-                    }
-                }
-                if (result[a].tier !== 'CHALLENGER' && result[a].tier !== 'MASTER' && result[a].tier !== 'GRANDMASTER')
-                    newData.rank = `${result[a].tier === ''} ${result[a].rank}`
-                else
-                    newData.rank = result[a].tier
-            }
-        })
-
-    await fetch(`https://${server}.api.riotgames.com/lol/champion-mastery/v4/champion-masteries/by-summoner/${newData.summonerId}`, {
-        method: 'GET',
-        mode: 'cors',
-        headers: {
-            'X-Riot-Token': process.env.RIOT_API_KEY
-        }
-    })
-        .then(result => result.json())
-        .then(result => newData.topChamps.push(result[0].championId, result[1].championId, result[2].championId))
+    let newData = await refresh(accountId, profile.summonerId, profile.summonerName, profile.server)
 
     try {
         await Profile.update(
@@ -254,6 +189,10 @@ router.put('/refresh', validateJWT, async (request, response) => {
         );
         response.status(200).json({
             message: "Updated",
+            rank: newData.rank,
+            topChamps: newData.topChamps,
+            summonerIcon: newData.summonerIcon,
+            level: newData.level
         });
     } catch (error) {
         response.status(500).json({
